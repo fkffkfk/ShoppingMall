84d80689d4fdeec5e9422963259338d612e0b2df
fkffkfk <minisnake@naver.com>
2023-06-23 01:58:30 +0900

    6.23 텀프3

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppRouter from './AppRouter';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AppRouter />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+/*<div id="root">
+<div class="App">
+  <header class ="App-header">
+    <img src="/static/media/logo.6ce24c58.svg" class="App-logo" alt="logo"></img>
+      <p>Edit <code>src/App.js</code> and save to relohttps://eslint.org/docs/latest/rules/no-octal-escapead. </p>
+      <a class="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+  </header>
+</div>
+
+</div>*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
